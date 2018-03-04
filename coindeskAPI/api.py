4b02d7406d103d00a299d2330dd9e25a78b733ba
Sergio de Diego <sergio.dediego@outlook.com>
2018-03-04 18:06:21 +0100

    Add CoinDesk API http request class

--- coindeskAPI/api.py
+++ coindeskAPI/api.py
@@ -7,3 +7,10 @@ class CoinDeskAPI(object):
     Enable CoinDesk API use.
     """
     pass
+
+
+class CoinDeskHttpRequestAPI(object):
+    """
+    Enable CoinDesk API data request.
+    """
+    pass
