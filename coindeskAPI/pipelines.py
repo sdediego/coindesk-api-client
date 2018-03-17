e5dee61511adfad112aacf9ae9126c00b5a33373
Sergio de Diego <sergio.dediego@outlook.com>
2018-03-17 20:19:11 +0100

    Add PostgreSQL and JSON file pipelines classes

--- coindeskAPI/pipelines.py
+++ coindeskAPI/pipelines.py
@@ -7,3 +7,17 @@ class MongoDBPipeline(object):
     Enable persist data in MongoDB.
     """
     pass
+
+
+class PostgreSQLPipeline(object):
+    """
+    Enable persist data in PostgreSQL database.
+    """
+    pass
+
+
+class JSONFileWriterPipeline(object):
+    """
+    Enable persist data in JSON file.
+    """
+    pass
