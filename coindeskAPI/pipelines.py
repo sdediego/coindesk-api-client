f0607e0cfc4ee08d930a30784602e792538f7fbf
Sergio de Diego <sergio.dediego@outlook.com>
2018-03-23 23:32:26 +0100

    Add json file writer pipeline dependencies

--- coindeskAPI/pipelines.py
+++ coindeskAPI/pipelines.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import logging
+import os
+
+from dotenv import load_dotenv
+from functools import partial
+from json import JSONDecoder
+from logging.config import fileConfig
+from os.path import basename, dirname, join
+
+from .exceptions import JSONFileWriterPipelineError
+
+# Custom logger for spiders module
+fileConfig(join(dirname(dirname(__file__)), 'logging.cfg'))
+logger = logging.getLogger(__name__)
+
+# Load .env file
+dotenv_path = join(dirname(dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
+
 
 class JSONFileWriterPipeline(object):
     """
@@ -8,7 +27,7 @@ class JSONFileWriterPipeline(object):
     """
     pass
 
-    
+
 class MongoDBPipeline(object):
     """
     Enable persist data in MongoDB.
