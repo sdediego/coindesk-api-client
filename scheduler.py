799933cbd73fb7cb208e46a0abbf3388736a768a
Sergio de Diego <sergio.dediego@outlook.com>
2018-04-25 23:26:50 +0200

    Create data retrieving function and scheduled jobs

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@
 import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import date
 from logging.config import fileConfig
 from os.path import dirname, join
 
@@ -17,19 +18,29 @@ logger = logging.getLogger(__name__)
 sched = BlockingScheduler()
 
 
+def retrieve_and_persit_data(data):
+    """
+    Get and save data from CoinDesk API.
+
+    :param str data: type of data (currentprice, historical).
+    """
+    pass
+
 @sched.scheduled_job(id='currentprice', trigger='interval', minutes=1)
 def currentprice_job():
     """
     Get and save current price data from CoinDesk API.
     """
-    pass
+    logger.info('Fetching current price data.')
+    retrieve_and_persit_data('currentprice')
 
 @sched.scheduled_job(id='historical', trigger='cron', day_of_week='mon-sun', hour=0)
 def historical_job():
     """
     Get and save historical price data from CoinDesk API.
     """
-    pass
+    logger.info('Fetching historical price data.')
+    retrieve_and_persit_data('historical')
 
 
 # Start queueing jobs
